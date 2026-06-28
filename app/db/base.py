e1a386eb395932f438604886d2422b5505f68060
MANISH CHAUDHARY <132375487+Chaudhary626@users.noreply.github.com>
2026-06-28 16:09:36 +0530

    Update base.py

--- app/db/base.py
+++ app/db/base.py
@@ -15,7 +15,6 @@ settings = get_settings()
 engine = create_async_engine(
     settings.database_url,
     echo=not settings.is_production,
-    pool_pre_ping=True,
     pool_recycle=3600,
     pool_size=10,
     max_overflow=20,
